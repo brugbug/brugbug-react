48bac9fdd45c0fa3593d75a41d2b3f0dc7324d01
brugbug <brugbug001@gmail.com>
2023-03-07 21:44:19 -0500

    Updates

---
+++ precache-manifest.9fcd494d0b80eee48836d8d4f1957e49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "42764054b9c85fdf0e4d",
+    "url": "/brugbug-react/static/css/main.e36e78c3.chunk.css"
+  },
+  {
+    "revision": "42764054b9c85fdf0e4d",
+    "url": "/brugbug-react/static/js/main.42764054.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/brugbug-react/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/brugbug-react/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "b745833bca5e3852d708",
+    "url": "/brugbug-react/static/js/runtime~main.b745833b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/brugbug-react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c801401f64b110d4ffeffadbbd0f9141",
+    "url": "/brugbug-react/index.html"
+  }
+];
