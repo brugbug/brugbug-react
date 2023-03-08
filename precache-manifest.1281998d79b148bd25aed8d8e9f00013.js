1fd91eb0563c607c87708ec365fc4ce15f79d160
brugbug <brugbug001@gmail.com>
2023-03-07 22:00:37 -0500

    Updates

---
+++ precache-manifest.1281998d79b148bd25aed8d8e9f00013.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ec25a52670f768aa814",
+    "url": "/brugbug-react/static/css/main.e36e78c3.chunk.css"
+  },
+  {
+    "revision": "4ec25a52670f768aa814",
+    "url": "/brugbug-react/static/js/main.4ec25a52.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/brugbug-react/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/brugbug-react/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "b745833bca5e3852d708",
+    "url": "/brugbug-react/static/js/runtime~main.b745833b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/brugbug-react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2678eee054aa0e93be9f3d6b51a070ca",
+    "url": "/brugbug-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed80f96718788d71d914b19b76155612.js"
+  "/brugbug-react/precache-manifest.1281998d79b148bd25aed8d8e9f00013.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/brugbug-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
